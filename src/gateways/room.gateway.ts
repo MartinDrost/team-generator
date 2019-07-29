0be29ff6db115d7a029fbdec20e7b230f854c7de
Martin Drost <info@martindrost.nl>
2019-07-29 21:14:11 +0200

    Fix for invalid socket connection

--- src/gateways/room.gateway.ts
+++ src/gateways/room.gateway.ts
@@ -21,6 +21,7 @@ export class RoomGateway {
 
   @SubscribeMessage(SocketEvent.JOIN_ROOM)
   joinRoom(client: Socket, payload: IJoinRoomPayload) {
+    payload.accessCode = payload.accessCode.toUpperCase();
     const room = this.roomService.getRoom(payload.accessCode);
     if (!room) {
       throw new WsException('Incorrect room code');
